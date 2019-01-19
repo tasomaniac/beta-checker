061e01f0143ddbbd0fd7576dc7a7169d7ab3a944
Taco <SkytkRSfan3895@gmail.com>
2019-01-18 21:52:22 -0500

    update dependencies and plugins with breaking changes, some other small misc changes
    Signed-off-by: Taco <SkytkRSfan3895@gmail.com>

--- app/src/main/java/com/tasomaniac/betachecker/ShareToCheckBeta.java
+++ app/src/main/java/com/tasomaniac/betachecker/ShareToCheckBeta.java
@@ -2,13 +2,13 @@ package com.tasomaniac.betachecker;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.customtabs.CustomTabsIntent;
-import android.support.v4.app.ShareCompat;
+import android.support.customtabs.CustomTabsIntent.Builder;
+import android.support.v4.app.ShareCompat.IntentReader;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
@@ -21,7 +21,7 @@ public class ShareToCheckBeta extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ShareCompat.IntentReader reader = ShareCompat.IntentReader.from(this);
+        final IntentReader reader = IntentReader.from(this);
         String appId = extractPlayIdFrom(reader);
 
         if (appId == null) {
@@ -30,7 +30,7 @@ public class ShareToCheckBeta extends Activity {
             return;
         }
 
-        Intent intent = new CustomTabsIntent.Builder()
+        Intent intent = new Builder()
                 .setToolbarColor(ContextCompat.getColor(this, R.color.apps_primary))
                 .build()
                 .intent;
@@ -60,7 +60,7 @@ public class ShareToCheckBeta extends Activity {
         return getPackageManager().queryIntentActivities(browserIntent, 0);
     }
 
-    private static String extractPlayIdFrom(ShareCompat.IntentReader reader) {
+    private static String extractPlayIdFrom(IntentReader reader) {
         return findPlayAppId(reader.getText());
     }
 
@@ -69,7 +69,8 @@ public class ShareToCheckBeta extends Activity {
         if (text == null) {
             return null;
         }
-        final Matcher matcher = Pattern.compile("https://play\\.google\\.com/store/apps/details\\?id=(.+)", Pattern.CASE_INSENSITIVE)
+        final Matcher matcher = Pattern.compile(
+                "https://play\\.google\\.com/store/apps/details\\?id=(.+)", Pattern.CASE_INSENSITIVE)
                 .matcher(text);
         if (matcher.find()) {
             return matcher.group(1);
@@ -80,7 +81,7 @@ public class ShareToCheckBeta extends Activity {
     public boolean isPackageInstalled(String packageName) {
         try {
             return getPackageManager().getApplicationInfo(packageName, 0).enabled;
-        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NameNotFoundException e) {
             return false;
         }
     }
